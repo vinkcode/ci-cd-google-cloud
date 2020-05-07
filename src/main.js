a49e2a151575a7f33349e83b5cf1e1a3b8a6d147
Andre Vink <andrevink@hotmail.com>
2020-05-07 16:57:00 +0200

    move home.html to /public/index.html (because of default from vue-cli web pack)

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Home from './views/Home'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+if (document.querySelector('#app')) {
+  new Vue({
+    components: {
+      Home
+    }
+  }).$mount('#app')
+}
+
+console.log('test');
