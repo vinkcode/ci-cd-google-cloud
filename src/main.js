4edd521e13e3108a5cbaed1d978c03fa349ee38a
Andre Vink <andrevink@hotmail.com>
2020-05-07 11:17:42 +0200

    fixed vue-ci default app template

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-/* import Vue from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -9,20 +9,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app') */
-
-import Vue from 'vue'
-import App from './App.vue'
-import Home from './views/Home'
-
-if (document.querySelector('#app')) {
-  new Vue({
-    components: {
-      App,
-      Home
-    },
-    render: h => h(Home)
-  }).$mount('#app')
-}
-
-console.log('test')
+}).$mount('#app')
